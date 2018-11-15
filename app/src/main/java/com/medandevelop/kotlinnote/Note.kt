0e0e23075de96fbee17262c50b491426799caae5
bonatua <bonatua.tas@gmail.com>
2018-11-15 10:15:27 +0700

    Initial commit

---
+++ app/src/main/java/com/medandevelop/kotlinnote/Note.kt
@@ -0,0 +1,7 @@
+package com.medandevelop.kotlinnote
+
+class Note(nodeID:Int,nodeName:String,nodeDes:String) {
+    var nodeID:Int?=nodeID
+    var nodeName:String?=nodeName
+    var nodeDes:String?=nodeDes
+}
